ba4b1f6e2faf6d21f1865f0ed0cd18568375b1c6
Brook Tarekegn <brooksideas@gmail.com>
2021-02-04 20:38:39 +0300

    Setup visjs with vue for video processor

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="red">
+    <h1>Dashboard</h1>
+    <timeline ref="timeline" :items="items" :groups="groups" :options="options">
+    </timeline>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  data: () => ({
+    groups: [{
+      	id: 0,
+        content: 'Group 1'
+      }],
+    items: [{
+      	id: 0,
+        group: 0,
+        start: new Date(),
+        content: 'Item 1'
+      }],
+    options: {
+      editable: true,
+    },
+  }),
+};
+</script>
+
+ 
